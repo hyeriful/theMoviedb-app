f801b371fa978ea93ff6c61d38a701f4995e9440
hyeriful <dlgpfl2523@gmail.com>
2020-09-30 01:52:57 +0900

    config

---
+++ client/src/components/Config.js
@@ -0,0 +1,5 @@
+export const USER_SERVER = "/api/users";
+export const API_URL = "https://api.themoviedb.org/3/";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
+
+export const API_KEY = "d8ebc9ef7034a50c60469b2c20bb46e1";
